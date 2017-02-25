c97e8a9e5d4558c9e9feee98cfca681c9fdc3721
Lxp <aa869410917bb@qq.com>
2017-02-25 13:40:30 +0800

    新view

--- application/views/index.php
+++ application/views/index.php
@@ -51,73 +51,16 @@
 
     <div class="pet_circle_nav">
         <ul class="pet_circle_nav_list">
-            <li><a href="" class="iconfont pet_nav_xinxianshi ">&#xe61e;</a><span>新鲜事</span></li>
-            <li><a href="" class="iconfont pet_nav_zhangzhishi ">&#xe607;</a><span>趣闻</span></li>
-            <li><a href="" class="iconfont pet_nav_kantuya ">&#xe62c;</a><span>阅读</span></li>
-            <li><a href="" class="iconfont pet_nav_mengzhuanti ">&#xe622;</a><span>专题</span></li>
-            <li><a href="" class="iconfont pet_nav_meirong ">&#xe629;</a><span>订阅</span></li>
-            <li><a href="" class="iconfont pet_nav_yiyuan ">&#xe602;</a><span>专栏</span></li>
-            <li><a href="" class="iconfont pet_nav_dianpu ">&#xe604;</a><span>讨论</span></li>
-            <li><a href="javascript:;" class="iconfont pet_nav_gengduo ">&#xe600;</a><span>更多</span></li>
+            <li><a href="" class="iconfont pet_nav_xinxianshi ">&#xe61e;</a><span>健康助手</span></li>
+            <li><a href="" class="iconfont pet_nav_zhangzhishi ">&#xe607;</a><span>个人信息</span></li>
+            <li><a href="" class="iconfont pet_nav_kantuya ">&#xe62c;</a><span>健康论坛</span></li>
+            <li><a href="javascript:;" class="iconfont pet_nav_gengduo ">&#xe600;</a><span>更多资讯</span></li>
         </ul>
         <div class="pet_more_list">
             <div class="pet_more_list_block">
                 <div class="iconfont pet_more_close">×</div>
                 <div class="pet_more_list_block">
-                    <div class="pet_more_list_block_name">
-                        <div class="pet_more_list_block_name_title">阅读 Read</div>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_xinxianshi pet_more_list_block_line_ico">&#xe61e;</i>
-                            <div class="pet_more_list_block_line_font">新鲜事</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_zhangzhishi pet_more_list_block_line_ico">&#xe607;</i>
-                            <div class="pet_more_list_block_line_font">趣闻</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_kantuya pet_more_list_block_line_ico">&#xe62c;</i>
-                            <div class="pet_more_list_block_line_font">阅读</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_mengzhuanti pet_more_list_block_line_ico">&#xe622;</i>
-                            <div class="pet_more_list_block_line_font">专题</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_bk pet_more_list_block_line_ico">&#xe629;</i>
-                            <div class="pet_more_list_block_line_font">订阅</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_wd pet_more_list_block_line_ico">&#xe602;</i>
-                            <div class="pet_more_list_block_line_font">专栏</div>
-                        </a>
-                        <div class="pet_more_list_block_name_title pet_more_list_block_line_height">服务 Service</div>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_xinxianshi pet_more_list_block_line_ico">&#xe61e;</i>
-                            <div class="pet_more_list_block_line_font">新鲜事</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_zhangzhishi pet_more_list_block_line_ico">&#xe607;</i>
-                            <div class="pet_more_list_block_line_font">趣闻</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_kantuya pet_more_list_block_line_ico">&#xe62c;</i>
-                            <div class="pet_more_list_block_line_font">阅读</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_mengzhuanti pet_more_list_block_line_ico">&#xe622;</i>
-                            <div class="pet_more_list_block_line_font">专题</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_bk pet_more_list_block_line_ico">&#xe629;</i>
-                            <div class="pet_more_list_block_line_font">订阅</div>
-                        </a>
-                        <a class="pet_more_list_block_line">
-                            <i class="iconfont pet_nav_wd pet_more_list_block_line_ico">&#xe602;</i>
-                            <div class="pet_more_list_block_line_font">专栏</div>
-                        </a>
-                    </div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -199,163 +142,6 @@
                             </a>
                         </div>
                     </li>
-
-                    <li class="am-g am-list-item-desced pet_list_one_block">
-                        <div class="pet_list_one_info">
-                            <div class="pet_list_one_info_l">
-                                <div class="pet_list_one_info_ico"><img src="assets/img/a3.png" alt=""></div>
-                                <div class="pet_list_one_info_name">养了猫的飞飞</div>
-                            </div>
-                            <div class="pet_list_one_info_r">
-                                <div class="pet_list_tag pet_list_tag_stj">阅读</div>
-                            </div>
-                        </div>
-                        <div class=" am-list-main">
-                            <h3 class="am-list-item-hd pet_list_one_bt"><a href="###" class="">浣熊孤儿掉到树下，被一家人收养之后……</a>
-                            </h3>
-                            <ul data-am-widget="gallery" class="am-gallery am-avg-sm-3
-  am-avg-md-3 am-avg-lg-3 am-gallery-default pet_list_one_list">
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/qq1.jpg" alt="某天 也许会相遇 相遇在这个好地方"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/qq2.jpg" alt="不要太担心 只因为我相信"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/qq3.jpg" alt="终会走过这条遥远的道路"/>
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                            <div class="am-list-item-text pet_list_two_text">巴哈马拿骚的居民Rosie
-                                Kemp发现一个刚出生的浣熊，掉在了树下。因为找不到小浣熊的妈妈了，Rosie和她的女儿Laura Young决定收养这只小东西，并给她取名“小南瓜”。
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="am-g am-list-item-desced pet_list_one_block">
-                        <div class="pet_list_one_info">
-                            <div class="pet_list_one_info_tytj"><i
-                                    class="iconfont pet_nav_kantuya pet_more_list_block_line_ico pet_list_tytj_ico">
-                                    &#xe607;</i>诺奖得主回忆通知获奖那一刻
-                            </div>
-                            <div class="pet_list_one_info_r">
-                                <div class="pet_list_tag pet_list_tag_kty">图集</div>
-                            </div>
-                        </div>
-                        <div class=" am-list-main">
-                            <ul data-am-widget="gallery" class="am-gallery am-avg-sm-3
-  am-avg-md-3 am-avg-lg-3 am-gallery-default pet_list_one_list pet_list_one_tytj">
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/w1.jpg" alt="某天 也许会相遇 相遇在这个好地方"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/w2.jpg" alt="不要太担心 只因为我相信"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/w3.jpg" alt="终会走过这条遥远的道路"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/w4.jpg" alt="终会走过这条遥远的道路"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/w5.jpg" alt="终会走过这条遥远的道路"/>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="am-gallery-item">
-                                        <a href="###" class="">
-                                            <img src="assets/img/w6.jpg" alt="终会走过这条遥远的道路"/>
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                    </li>
-                    <li class="am-g am-list-item-desced pet_list_one_block">
-                        <div class="pet_list_one_info">
-                            <div class="pet_list_one_info_l">
-                                <div class="pet_list_one_info_ico"><img src="assets/img/a4.png" alt=""></div>
-                                <div class="pet_list_one_info_name">大兔</div>
-                            </div>
-                            <div class="pet_list_one_info_r">
-                                <div class="pet_list_tag pet_list_tag_zzs">趣闻</div>
-                            </div>
-                        </div>
-                        <div class=" am-list-main">
-                            <h3 class="am-list-item-hd pet_list_one_bt"><a href="###" class="">“你的旅行，是什么颜色？”
-                                    晒照片，换北欧梦幻极光之旅！</a></h3>
-
-
-                            <div class="am-list-item-text pet_list_two_text">
-                                狗狗会天天的跟着我们生活在一起，它们的一切都会影响着主人，尤其是狗狗身上散发的味道，会无时无刻的对主人有影响，如果狗狗身体有异味，主人就会用过于香喷喷的洗漱品帮狗狗洗澡，这样不仅对狗狗身体有伤害，还会容易患上皮肤病，其实，我们知道一些小技巧，就会改善狗狗身上存在的味道。
-                            </div>
-
-                        </div>
-                    </li>
-                    <li class="am-g am-list-item-desced pet_list_one_block">
-                        <div class="pet_list_one_info">
-                            <div class="pet_list_one_info_l">
-                                <div class="pet_list_one_info_ico"><img src="assets/img/a5.png" alt=""></div>
-                                <div class="pet_list_one_info_name">Hope</div>
-                            </div>
-                            <div class="pet_list_one_info_r">
-                                <div class="pet_list_tag pet_list_tag_mzt">萌专题</div>
-                            </div>
-                        </div>
-                        <div class=" am-list-main">
-                            <h3 class="am-list-item-hd pet_list_one_bt"><a href="###" class="">心情不好了，就来看看这只狗！</a></h3>
-                            <div class="pet_list_zt_img"><img src="assets/img/c1.png" alt=""></div>
-
-                            <div class="am-list-item-text pet_list_two_text">
-                                猫咪不像人，猫咪的情绪不会写在脸上，反馈给我们的信息更多的应该是行为上肢体上的，当然从叫声中也会反应一些信息，那么要想“抓住它的心，就一定要抓住它的胃吗？”从它的行为和肢体语言当中我们可以读懂什么呢？
-                            </div>
-
-                        </div>
-                    </li>
-                    <li class="am-g am-list-item-desced pet_list_one_block">
-                        <div class="pet_article_user_block">
-                            <div class="pet_article_user_img">
-                                <div class="pet_article_user_shadow"></div>
-                                <div class="pet_article_user_title">一周在任意地点工作三天？</div>
-                                <img src="assets/img/c2.png" alt=""></div>
-                            <div class="pet_article_user_info">
-                                <div class="pet_article_user_info_ico"><img src="assets/img/c.png" alt=""></div>
-                                SeeYouAgain
-                            </div>
-                            <div class="am-list-item-text pet_article_user_nr">
-                                这是一家帮助客户在亚太地区找到适合的打折酒店的中介机构，在全球设立了9个办事处，老板克里斯蒂安·米施勒宣称要把它打造成世界上最棒的公司。
-                            </div>
-                        </div>
-                    </li>
                 </ul>
             </div>
 
@@ -363,19 +149,7 @@
 
     </div>
     <div class="pet_article_dowload pet_dowload_more_top_off">
-        <div class="pet_article_dowload_title">关于Amaze UI</div>
-        <div class="pet_article_dowload_content pet_dowload_more_top_bg">
-            <div class="pet_article_dowload_triangle pet_dowload_more_top_san"></div>
-            <div class="pet_article_dowload_ico"><img src="assets/img/footdon.png" alt=""></div>
-            <div class="pet_article_dowload_content_font">
-                Amaze UI 以移动优先（Mobile first）为理念，从小屏逐步扩展到大屏，最终实现所有屏幕适配，适应移动互联潮流。
-            </div>
-            <div class="pet_article_dowload_all">
-                <a href="###" class="pet_article_dowload_Az am-icon-apple"> App store</a>
-                <a href="###" class="pet_article_dowload_Pg am-icon-android"> Android</a>
-            </div>
-        </div>
-        <div class="pet_article_footer_info">Copyright(c)2015 Amaze UI All Rights Reserved</div>
+        <div class="pet_article_footer_info">Copyright(c)2017 By 毕业设计</div>
     </div>
 </div>
 
