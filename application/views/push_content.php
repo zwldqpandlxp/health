86bfab3891886aa2918ab0ff2aea64aae137da85
Lxp <aa869410917bb@qq.com>
2017-02-28 19:17:14 +0800

    push内容页

---
+++ application/views/push_content.php
@@ -0,0 +1,342 @@
+<!doctype html>
+<html>
+<head>
+    <base href="<?php echo site_url() ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link rel="stylesheet" href="assets/css/amazeui.min.css">
+    <link rel="stylesheet" href="assets/css/wap.css">
+    <title>内容页</title>
+</head>
+<body style="background:#ececec">
+<div class="pet_mian" >
+    <div class="pet_head">
+  <header data-am-widget="header"
+          class="am-header am-header-default pet_head_block">
+      <div class="am-header-left am-header-nav ">
+          <a href="#left-link" class="iconfont pet_head_jt_ico">&#xe601;</a>
+      </div>
+
+      <h1 class="am-header-title pet_article_user">
+      <div class="pet_article_user_info_tab">
+        <div class="pet_article_user_info_tab_font">
+        <div class="pet_article_user_info_tab_font_triangle"></div>
+        <div class="pet_article_user_info_tab_font_center"><i>文章</i><span>16篇</span></div>
+<!--         <div class="pet_article_user_info_tab_font_center"><i>涂鸦</i><span>89张</span></div>
+        <div class="pet_article_user_info_tab_font_center_line"></div>
+        <div class="pet_article_user_info_tab_font_center"><a href="###">查看全部资料</a></div> -->
+        </div>
+      </div>
+      <span class="pet_article_user_ico"><img src="assets/img/a1.png" alt="" class=""></span>
+      <span class="pet_article_user_name">EGOIST</span>
+      </h1>
+
+      <div class="am-header-right am-header-nav">
+          <a href="javascript:;" class="iconfont pet_head_gd_ico">&#xe600;</a>
+      </div>
+  </header>
+</div>
+
+
+
+    <div class="pet_more_list"><div class="pet_more_list_block">
+    <div class="iconfont pet_more_close">×</div>
+    <div class="pet_more_list_block">
+        <div class="pet_more_list_block_name">
+            <div class="pet_more_list_block_name_title">阅读 Read</div>
+            <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_xinxianshi pet_more_list_block_line_ico">&#xe601;</i>
+                <div class="pet_more_list_block_line_font">新鲜事</div>
+            </a>
+                        <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_zhangzhishi pet_more_list_block_line_ico">&#xe606;</i>
+                <div class="pet_more_list_block_line_font">涨知识</div>
+            </a>
+                        <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_kantuya pet_more_list_block_line_ico">&#xe607;</i>
+                <div class="pet_more_list_block_line_font">看涂鸦</div>
+            </a>
+                        <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_mengzhuanti pet_more_list_block_line_ico">&#xe603;</i>
+                <div class="pet_more_list_block_line_font">萌专题</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_bk pet_more_list_block_line_ico">&#xe611;</i>
+                <div class="pet_more_list_block_line_font">百科</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_wd pet_more_list_block_line_ico">&#xe60c;</i>
+                <div class="pet_more_list_block_line_font">问答</div>
+            </a>
+ <div class="pet_more_list_block_name_title pet_more_list_block_line_height">服务 Service</div>
+            <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_xinxianshi pet_more_list_block_line_ico">&#xe600;</i>
+                <div class="pet_more_list_block_line_font">美容</div>
+            </a>
+                        <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_zhangzhishi pet_more_list_block_line_ico">&#xe602;</i>
+                <div class="pet_more_list_block_line_font">医院</div>
+            </a>
+                        <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_kantuya pet_more_list_block_line_ico">&#xe604;</i>
+                <div class="pet_more_list_block_line_font">店铺</div>
+            </a>
+                        <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_sy pet_more_list_block_line_ico">&#xe610;</i>
+                <div class="pet_more_list_block_line_font">摄影</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_xx pet_more_list_block_line_ico">&#xe612;</i>
+                <div class="pet_more_list_block_line_font">学校</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_sz pet_more_list_block_line_ico">&#xe60f;</i>
+                <div class="pet_more_list_block_line_font">水族</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_ms pet_more_list_block_line_ico">&#xe60e;</i>
+                <div class="pet_more_list_block_line_font">猫舍</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_qs pet_more_list_block_line_ico">&#xe60b;</i>
+                <div class="pet_more_list_block_line_font">犬舍</div>
+            </a>
+                                    <a class="pet_more_list_block_line">
+                <i class="iconfont pet_nav_qt pet_more_list_block_line_ico">&#xe60d;</i>
+                <div class="pet_more_list_block_line_font">其它</div>
+            </a>
+        </div>
+    </div>
+
+    </div></div>
+
+
+
+<div class="pet_content">
+<div class="pet_content_block">
+  <article data-am-widget="paragraph" class="am-paragraph am-paragraph-default pet_content_article" data-am-paragraph="{ tableScrollable: true, pureview: true }">
+  <h1 class="pet_article_title">你认识萌萌的小熊猫吗？</h1>
+  <div class="pet_article_user_time">发表于：2015-09-25</div>
+      <img src="assets/img/a2.jpg"><p class=paragraph-default-p>南极洲又称<a href="###">第七大陆</a>，是地球上最后一个被发现、唯一没有土著人居住的大陆。</p><p>南极大陆为通常所说的南大洋（太平洋、印度洋和大西洋的南部水域）所包围，它与南美洲最近的距离为965公里，距新西兰2000公里、距澳大利亚2500公里、距南非3800公里、距中国北京的距离约有12000公里。南极大陆的总面积为1390万平方公里，相当于中国和印巴次大陆面积的总和，居世界各洲第五位。</p><p><blockquote><p>有一天我们也要去南极走走！！</p></blockquote></p><img src="assets/img/a3.jpg" /><p>整个南极大陆被一个巨大的冰盖所覆盖，平均海拔为2350米。南极洲是由冈瓦纳大陆分离解体而成，是世界上最高的大陆。南极横断山脉将南极大陆分成东西两部分。这两部分在地理和地质上差别很大。</p><img src="assets/img/a2.jpg" /><p>东南极洲是一块很古老的大陆，据科学家推算,已有几亿年的历史。它的中心位于难接近点，从任何海边到难接近点的距离都很远。东南极洲平均海拔高度2500米，最大高度4800 米。在东南极洲有南极大陆最大的活火山，即位于罗斯岛上的埃里伯斯火山，海拔高度3795米，有四个喷火口</p>
+
+  </article>
+        <ul class="like_share_block">
+        <li><a class="link_share_button" href="###"><i class="iconfont share_ico_link">&#xe62f;</i>1460</a></li>
+        <li><a class="link_share_button" href="###"><i class="iconfont share_ico_wx">&#xe630;</i>微信</a></li>
+        <li><a class="link_share_button" href="###"><i class="iconfont share_ico_pyq">&#xe62e;</i>朋友圈</a></li>
+      </ul>
+    <div class="pet_article_dowload">
+      <div class="pet_article_dowload_title">关于Amaze UI</div>
+      <div class="pet_article_dowload_content"><div class="pet_article_dowload_triangle"></div>
+      <div class="pet_article_dowload_ico"><img src="assets/img/footdon.png" alt=""></div>
+      <div class="pet_article_dowload_content_font">
+Amaze UI 以移动优先（Mobile first）为理念，从小屏逐步扩展到大屏，最终实现所有屏幕适配，适应移动互联潮流。
+      </div>
+      <div class="pet_article_dowload_all">
+        <a href="###" class="pet_article_dowload_Az am-icon-apple"> App store</a>
+        <a href="###" class="pet_article_dowload_Pg am-icon-android"> Android</a>
+      </div>
+      </div>
+  </div>
+</div>
+<div class="pet_comment_list">
+  <div class="pet_comment_list_wap"><div class="pet_comment_list_title">精彩评论</div>
+
+  <div data-am-widget="tabs" class="am-tabs am-tabs-default pet_comment_list_tab" >
+      <ul class="am-tabs-nav am-cf pet_comment_list_title_tab">
+          <li class="am-active"><a href="[data-tab-panel-0]">人气</a></li>
+          <li class=""><a href="[data-tab-panel-1]">最新</a></li>
+          <li class=""><a href="[data-tab-panel-2]">最早</a></li>
+      </ul>
+      <div class="am-tabs-bd pet_pl_list">
+          <div data-tab-panel-0 class="am-tab-panel am-active">
+            <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a1.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">Amaze</div>
+                <div class="pet_comment_list_block_r_text">那时候有多好，任雨打湿裙角。忍不住哼起，心爱的旋律。绿油油的树叶，自由地在说笑。燕子忙归巢，风铃在舞蹈。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">10分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+                        <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a2.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">一如既往</div>
+                <div class="pet_comment_list_block_r_text"><span>@Drary</span>没有老友你的陪伴，日子真是漫长与你重逢之时，我会敞开心扉倾诉所有。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">15分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+                        <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a3.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">AllThe</div>
+                <div class="pet_comment_list_block_r_text">表紙の裏に移らないよに あけた一枚目のページの裏に透けた文字めくろうとして 指がとまって</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">30分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+                        <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a4.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">Drary</div>
+                <div class="pet_comment_list_block_r_text">无论历经怎样的艰难坎坷，总有你相伴陪我度过。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">1小时前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div data-tab-panel-1 class="am-tab-panel ">
+                                  <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a2.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">一如既往</div>
+                <div class="pet_comment_list_block_r_text"><span>@Drary</span>没有老友你的陪伴，日子真是漫长与你重逢之时，我会敞开心扉倾诉所有。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">15分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+                       <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a3.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">AllThe</div>
+                <div class="pet_comment_list_block_r_text">表紙の裏に移らないよに あけた一枚目のページの裏に透けた文字めくろうとして 指がとまって</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">30分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+                    <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a1.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">Amaze</div>
+                <div class="pet_comment_list_block_r_text">那时候有多好，任雨打湿裙角。忍不住哼起，心爱的旋律。绿油油的树叶，自由地在说笑。燕子忙归巢，风铃在舞蹈。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">10分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+
+
+                        <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a4.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">Drary</div>
+                <div class="pet_comment_list_block_r_text">无论历经怎样的艰难坎坷，总有你相伴陪我度过。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">1小时前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div data-tab-panel-2 class="am-tab-panel ">
+                                <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a1.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">Amaze</div>
+                <div class="pet_comment_list_block_r_text">那时候有多好，任雨打湿裙角。忍不住哼起，心爱的旋律。绿油油的树叶，自由地在说笑。燕子忙归巢，风铃在舞蹈。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">10分钟前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+
+
+                        <div class="pet_comment_list_block">
+              <div class="pet_comment_list_block_l"><img src="assets/img/a4.png" alt=""></div>
+              <div class="pet_comment_list_block_r">
+                <div class="pet_comment_list_block_r_info">Drary</div>
+                <div class="pet_comment_list_block_r_text">无论历经怎样的艰难坎坷，总有你相伴陪我度过。</div>
+                <div class="pet_comment_list_block_r_bottom">
+                  <div class="pet_comment_list_bottom_info_l">1小时前</div>
+                  <div class="pet_comment_list_bottom_info_r">
+                  <span><i class="iconfont">&#xe631;</i>5 </span>
+                  <span> 回复</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+
+
+
+
+  </div>
+</div>
+
+<div class="pet_article_footer_info">Copyright(c)2017 By 毕业设计</div>
+</div>
+</div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/amazeui.min.js"></script>
+<script>
+$(function(){
+
+    // 动态计算新闻列表文字样式
+    auto_resize();
+    $(window).resize(function() {
+        auto_resize();
+    });
+    $('.am-list-thumb img').load(function(){
+        auto_resize();
+    });
+    $('.pet_article_like li:last-child').css('border','none');
+        function auto_resize(){
+        $('.pet_list_one_nr').height($('.pet_list_one_img').height());
+                // alert($('.pet_list_one_nr').height());
+    }
+        $('.pet_article_user').on('click',function(){
+            if($('.pet_article_user_info_tab').hasClass('pet_article_user_info_tab_show')){
+                $('.pet_article_user_info_tab').removeClass('pet_article_user_info_tab_show').addClass('pet_article_user_info_tab_cloes');
+            }else{
+                $('.pet_article_user_info_tab').removeClass('pet_article_user_info_tab_cloes').addClass('pet_article_user_info_tab_show');
+            }
+        });
+
+        $('.pet_head_gd_ico').on('click',function(){
+            $('.pet_more_list').addClass('pet_more_list_show');
+       });
+        $('.pet_more_close').on('click',function(){
+            $('.pet_more_list').removeClass('pet_more_list_show');
+        });
+});
+
+</script>
+</body>
+</html>
